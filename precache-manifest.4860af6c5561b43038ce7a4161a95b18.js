34b79817d0d72bb62ad13beb61614bdc2ab8bb21
Reenya <reenya577@gmail.com>
2019-04-19 00:41:56 +0300

    Updates

---
+++ precache-manifest.4860af6c5561b43038ce7a4161a95b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.6670768c.js"
+  },
+  {
+    "revision": "46f84e85e78d64d07f53",
+    "url": "/TicTacToe/static/js/main.46f84e85.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/TicTacToe/static/js/2.5694e189.chunk.js"
+  },
+  {
+    "revision": "46f84e85e78d64d07f53",
+    "url": "/TicTacToe/static/css/main.1fea592b.chunk.css"
+  },
+  {
+    "revision": "749111625e8241bcbcb7c99669f22e48",
+    "url": "/TicTacToe/index.html"
+  }
+];
